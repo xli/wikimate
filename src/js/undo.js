24818ee4507f0f0c308fc3fe3b5d3e4a15d82950
Xiao Li <swing1979@gmail.com>
2012-04-22 23:00:13 -0700

    changed to use switch cases, clean code

--- src/js/undo.js
+++ src/js/undo.js
@@ -1,31 +1,26 @@
 (function($) {
-  var revert = (function() {
-    var revert_action_type_map = {'remove': 'add', 'add': 'remove', 'edit': 'edit'};
-    var map = {
-      remove: function(action) {
-        var story = wikimate.utils.replay(this.wikimate('journal'));
+  function revert(action) {
+    var story;
+    switch(action.type) {
+      case 'remove':
+        story = wikimate.utils.replay(this.wikimate('journal'));
         var item = story.itemById(action.id);
         var index = story.indexOf(item);
         var after = index === 0 ? null : story[index - 1].id;
         return {id: action.id, type: 'add', item: item, after: after};
-      },
-      add: function(action) {
-        return {id: action.id, type: 'remove', item: action.item, after: action.after};
-      },
-      edit: function(action) {
-        var story = wikimate.utils.replay(this.wikimate('journal'));
+      case 'add':
+        return {id: action.id, type: 'remove', item: action.item};
+      case 'edit':
+        story = wikimate.utils.replay(this.wikimate('journal'));
         var prev = story.itemById(action.id);
         return {id: action.id, type: 'edit', item: $.extend({}, prev)};
-      },
-      move: function(action) {
-        var story = wikimate.utils.replay(this.wikimate('journal'));
+      case 'move':
+        story = wikimate.utils.replay(this.wikimate('journal'));
         return {id: action.id, type: 'move', order: _.pluck(story, 'id')};
-      }
-    };
-    return function(action) {
-      return map[action.type].apply(this, [action]);
-    };
-  })();
+      default:
+        throw "Unknown action type: " + action.type;
+    }
+  }
 
   wikimate.fn.undo = function() {
     var action = this.find('.wikimate-journal').journal('pop');
